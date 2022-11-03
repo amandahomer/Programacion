6016dbb487898162f300c57e301b9023620f5403
amandahomer <117313215+amandahomer@users.noreply.github.com>
2022-11-03 16:04:40 +0100

    Create EjercicioLibro2.java

---
+++ EjercicioLibro2.java
@@ -0,0 +1,14 @@
+/*Este programa muestra mi nombre, mi direccion y mi telefono por pantalla
+@AmandaNR */
+
+public class EjercicioLibro2 {
+
+	public static void main (String[] args) {
+		
+		System.out.println("Amanda");
+		System.out.println("Calle Catedratico Cristobal Cuevas");
+		System.out.println("623187292");
+
+
+	}
+}
